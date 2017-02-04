4176a9548d0dcef7a7c35864593905beadbfa338
Matt Burke <spraints@gmail.com>
2017-02-04 16:27:40 +0100

    Make /about* more static

--- site.js
+++ site.js
@@ -246,64 +246,6 @@ var Downloads = {
   }
 }
 
-var AboutContent = {
-  defaultSection: "branching-and-merging",
-
-  init: function() {
-    if ($('body#about').length === 0) return;
-    $('section.about').hide();
-    $('section.about .bottom-nav').show();
-    AboutContent.observeNav();
-    AboutContent.observePopState();
-    AboutContent.showSection(AboutContent.getSection());
-  },
-
-  observePopState: function() {
-    if (window.history && window.history.pushState) {
-      return $(window).bind('popstate', function(event) {
-        var section;
-        initialPop = !popped && location.href === initialURL;
-        popped = true;
-        if (initialPop) {
-          return;
-        }
-        section = AboutContent.getSection();
-        return AboutContent.showSection(section);
-      });
-    }
-  },
-
-  getSection: function(href) {
-    var section;
-    section = location.href.substring(location.href.lastIndexOf("/") + 1);
-    if (section.length === 0 || section == 'about') {
-      section = AboutContent.defaultSection;
-    }
-    return section;
-  },
-
-  showSection: function(section) {
-    if (section == 'about') section = AboutContent.defaultSection;
-    $('ol#about-nav a').removeClass('current');
-    $('ol#about-nav a#nav-' + section).addClass('current');
-    $('section').hide(0, function(){
-      $('section#' + section).show();
-    });
-  },
-
-  observeNav: function() {
-    $('ol#about-nav a, .bottom-nav a').click(function(e) {
-      e.preventDefault();
-      var section = $(this).attr('data-section-id');
-
-      if (window.history && window.history.pushState) {
-        history.pushState(null, $(this).html(), '/about/'+section);
-      }
-      AboutContent.showSection(section);
-    });
-  }
-}
-
 var FlippyBook = {
   threeDee: false,
 
